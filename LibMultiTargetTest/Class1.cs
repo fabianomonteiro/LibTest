68a5e16724d7042c50b10b637604f6050d6bc36e
Fabiano Monteiro <fabianomonteiro@outlook.com>
2022-03-21 19:58:00 -0300

    Teste

--- LibMultiTargetTest/Class1.cs
+++ LibMultiTargetTest/Class1.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using System.Net.Http;
 
 namespace LibMultiTargetTest
 {
     public class Class1
     {
-
+        public void Teste()
+        {
+            var httpCLient = new HttpClient();
+            var newtonsoft = new Newtonsoft.Json.JsonSerializer();
+            var restClient = new RestClient();
+        }
     }
 }
